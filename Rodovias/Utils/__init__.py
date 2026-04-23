3533ac5037a36389986f92ef1ed095b77b129612
TiagosCompanny <116459741+TiagosCompanny@users.noreply.github.com>
2026-04-23 18:37:40 -0300

    feat: implementar LogService CSV diário por tipo

---
+++ Rodovias/Utils/__init__.py
@@ -0,0 +1,19 @@
+from .gerenciador_log_apostas import (
+    ExecucaoLog,
+    LogService,
+    OrdemLog,
+    PrevisaoLog,
+    ResultadoLog,
+    TipoLog,
+    TreinamentoLog,
+)
+
+__all__ = [
+    "TipoLog",
+    "LogService",
+    "PrevisaoLog",
+    "OrdemLog",
+    "ResultadoLog",
+    "ExecucaoLog",
+    "TreinamentoLog",
+]
